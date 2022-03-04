29fb58c52c3fb428ae70ee6c1ca65bd434672db7
pzaidins2 <paulzaidins2@gmail.com>
2022-03-03 22:31:37 -0500

    Update gather_resources.sh

--- hw2/gather_resources.sh
+++ hw2/gather_resources.sh
@@ -6,5 +6,5 @@ wget "https://cmsc848q-hw2-group11.s3.amazonaws.com/answer_extractor.zip"
 unzip answer_extractor.zip
 mv answer_extractor/ models/
 
-wget "https://cmsc848q-hw2-group11.s3.amazonaws.com/custom_data.zip"
-unzip custom_data.zip
+#wget "https://cmsc848q-hw2-group11.s3.amazonaws.com/custom_data.zip"
+#unzip custom_data.zip
